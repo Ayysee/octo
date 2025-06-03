bd5cdc91bca88d56a54d6fe51fd321aa71f95018
Franco Logozzo <franco.logozzo@gmail.com>
2025-06-02 22:08:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+player.onChat("run", function () {
+	
+})
+mobs.enchant(
+mobs.target(LOCAL_PLAYER),
+"Power",
+5
+)
+mobs.enchant(
+mobs.target(LOCAL_PLAYER),
+"Flame",
+1
+)
+mobs.enchant(
+mobs.target(LOCAL_PLAYER),
+"Unbreaking",
+3
+)
+mobs.enchant(
+mobs.target(LOCAL_PLAYER),
+"Mending",
+1
+)
